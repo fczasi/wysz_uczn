ba5656204e94a5116183d608adb26e0c6ea0ee90
fczasi <jetcrab893@gmail.com>
2022-09-16 11:25:17 +0200

    df

--- wysz_uczn/Form1.Designer.cs
+++ wysz_uczn/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
             this.textBox1.Location = new System.Drawing.Point(274, 32);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -55,7 +55,7 @@
             this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
             this.textBox2.Location = new System.Drawing.Point(274, 77);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(211, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
@@ -63,7 +63,7 @@
             this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
             this.textBox3.Location = new System.Drawing.Point(274, 207);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(211, 20);
             this.textBox3.TabIndex = 2;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
