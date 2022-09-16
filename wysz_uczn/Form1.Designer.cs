8e6018b90c997a9a40ee8b313d75760ab3d8d41a
fczasi <jetcrab893@gmail.com>
2022-09-16 11:25:55 +0200

    ..

--- wysz_uczn/Form1.Designer.cs
+++ wysz_uczn/Form1.Designer.cs
@@ -131,9 +131,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(149, 196);
+            this.pictureBox1.Location = new System.Drawing.Point(149, 207);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 31);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
